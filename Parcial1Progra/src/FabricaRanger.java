fcd1419cc7e47ee801a1ef2c7a32788db06ac1b0
La Fuente <La Fuente@DESKTOP-IBFARMP>
2017-09-11 23:27:56 -0600

    ParcialGaláctico

---
+++ Parcial1Progra/src/FabricaRanger.java
@@ -0,0 +1,12 @@
+
+
+
+public interface FabricaRanger<E> {
+	
+	public boolean apilar();
+    public boolean desapilar();
+    public boolean encolar();
+    public boolean desencolar();
+    public Monstruo fabricar();
+
+}
